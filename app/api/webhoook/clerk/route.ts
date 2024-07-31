446e78b47fd21d644b881db59172709a1e96b0ba
saloniTech <salonipatil10122001@gmail.com>
2024-07-31 13:19:53 +0530

    commit

--- app/api/webhoook/clerk/route.ts
+++ app/api/webhoook/clerk/route.ts
@@ -1,7 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
-import  { createUser,  updateUser , deleteUser} from '@/lib/actions/user.actions'
+import  { createUser,  updateUser , deleteUser} from '../../../../lib/actions/user.actions'
 import { NextResponse } from 'next/server'
 export async function POST(req: Request) {
 
@@ -78,7 +78,6 @@ export async function POST(req: Request) {
      return NextResponse.json({ message: 'OK', user: newUser })
   }
   if (eventType === 'user.updated') {
-
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
